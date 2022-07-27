e0adacae28c4615d6a0de697416428efcb5332e5
New Developer <new.dev@continental-corporation.com>
2022-07-27 10:43:57 +0200

    Fix bug in calculate dist function
    use y-coord correctly

--- src/point_calculator.cpp
+++ src/point_calculator.cpp
@@ -2,5 +2,5 @@
 #include <cmath>
 
 float PointCalculator::calcDistance(Point2D pt1, Point2D pt2) {
-  return std::sqrt((pt1.x - pt2.x) * (pt1.x - pt2.x) + (pt1.y - pt2.x) * (pt1.y - pt2.x));
+  return std::sqrt((pt1.x - pt2.x) * (pt1.x - pt2.x) + (pt1.y - pt2.y) * (pt1.y - pt2.y));
 }
